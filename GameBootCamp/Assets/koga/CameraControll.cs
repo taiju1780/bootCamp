f206f28b23e4a176ea134a301f62dc2e58fdc6cf
tekito <tekito@kk.kk>
2019-11-04 20:10:48 +0900

    firstpush

---
+++ GameBootCamp/Assets/koga/CameraControll.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControll : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+    public Transform verRot;
+    public Transform horRot;
+    //カメラとプレイヤーとの差分
+    private Vector3 offset;
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        verRot = transform.parent;
+        horRot = GetComponent<Transform>();
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (Mathf.Approximately(Time.timeScale, 0f))
+        {
+            return;
+        }
+
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ GameBootCamp/Assets/koga/Move.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Move : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private bool rotaright;
+    private bool rotaleft;
+    private bool rotaup;
+    private bool rotadown;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotaHorizontal = Input.GetAxis("Horizontal2");
+        float rotaVertical = Input.GetAxis("Vertical2");
+
+        if (rotaHorizontal == 1.0f)
+        {
+        }
+        if (rotaHorizontal == -1.0f)
+        {
+
+        }
+        if (rotaVertical == 1.0f)
+        {
+
+        }
+        if (rotaVertical == -1.0f)
+        {
+
+        }
+
+
+        transform.Rotate(new Vector3(0, rotaHorizontal, 0));
+    }
+}
