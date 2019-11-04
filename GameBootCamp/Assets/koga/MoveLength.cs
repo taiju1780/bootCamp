f1996c457c8e16e832b7db46a4f1f558ce651887
tekito <tekito@kk.kk>
2019-11-04 20:37:23 +0900

    移動距離の表示

---
+++ GameBootCamp/Assets/koga/MoveLength.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveLength : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject movetext;
+    [SerializeField] private Vector3 keeppos;
+    [SerializeField] private Vector3 movepos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        keeppos = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movepos = player.transform.position - keeppos;
+        DrawMovePos();
+    }
+
+    private void DrawMovePos()
+    {
+        Text move = movetext.GetComponent<Text>();
+        move.text = "移動距離" + movepos.x;
+    }
+}
